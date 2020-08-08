e89fd583b46b8b48cde1a13bcfef89b26857438d
Flor Mabel Contreras Rodriguez <usis011120@ugb.edu.sv>
2020-08-08 16:55:12 -0600

    Creación de ejercicio de las tablas

--- HolaMundo/HolaMundo/Form1.vb
+++ HolaMundo/HolaMundo/Form1.vb
@@ -1,46 +1,15 @@
 ﻿Public Class Form1
-    Private Sub btncalcular_Click(sender As Object, e As EventArgs) Handles btncalcular.Click
-        Dim num1, num2, respuesta As Double
-        num1 = txtnum1.Text
-        num2 = txtnum2.Text
-        If optsuma.Checked Then
-            lblrespuesta.Text = num1 + num2
+    Private Sub btnProcesar_Click(sender As Object, e As EventArgs) Handles btnProcesar.Click
+        Dim ntabla, i As SByte
+        ntabla = txtntabla.Text
+        lstTablas.Items.Clear()
+        If ntabla <= 12 Then
+            For i = 1 To 10
+                lstTablas.Items.Add(ntabla.ToString() + "x" + i.ToString() + "=" + (ntabla * i).ToString())
+            Next
+        Else
+            lstTablas.Items.Add("Por favor ingrese solo las tablas menores o iguales a 12.")
         End If
-        If optresta.Checked Then
-            lblrespuesta.Text = num1 - num2
-        End If
-        If optmultiplicacion.Checked Then
-            lblrespuesta.Text = num1 * num2
-        End If
-        If optdivicion.Checked Then
-            lblrespuesta.Text = num1 / num2
-        End If
-        If optporcentaje.Checked Then
-            lblrespuesta.Text = num1 * num2 / 100
-        End If
-        If optesponente.Checked Then
-            lblrespuesta.Text = num1 ^ num2
-        End If
-        If optmodulo.Checked Then
-            lblrespuesta.Text = num1 Mod num2
-        End If
-
-        Select Case cboOperaciones.SelectedIndex
-            Case 1
-                lblrespuesta.Text = num1 + num2
-            Case 2
-                lblrespuesta.Text = num1 - num2
-            Case 3
-                lblrespuesta.Text = num1 * num2
-            Case 4
-                lblrespuesta.Text = num1 / num2
-            Case 5
-                lblrespuesta.Text = num1 * num2 / 100
-            Case 6
-                lblrespuesta.Text = num1 ^ num2
-            Case 7
-                lblrespuesta.Text = num1 Mod num2
-        End Select
-
     End Sub
+
 End Class
