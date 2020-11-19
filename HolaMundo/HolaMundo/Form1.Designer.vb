a200787d6492151a6c465c99e1d1dca20d49f11e
Flor Mabel Contreras Rodríguez <usis011120@ugb.edu.sv>
2020-11-19 17:29:03 -0600

    Base de Datos

--- HolaMundo/HolaMundo/Form1.Designer.vb
+++ HolaMundo/HolaMundo/Form1.Designer.vb
@@ -28,7 +28,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1055, 523)
+        Me.ClientSize = New System.Drawing.Size(801, 466)
         Me.Name = "Form1"
         Me.Text = "Sistema de Inventario y Facturacion"
         Me.ResumeLayout(False)
